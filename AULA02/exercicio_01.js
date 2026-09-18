8da62e92620a55871de2697a9ca3b36ec34595c3
Brayan <brayanpontual73@gmail.com>
2026-09-18 11:11:42 -0300

    exercicio01

--- AULA02/exercicio_01.js
+++ AULA02/exercicio_01.js
@@ -1,3 +1,11 @@
 // Atividade Number
 // crie uma variável chama anoAtual e outra chamada anoNascimento.
-// calucule e exiba a idade
+// calucule e exiba a idade no(a) console.
+
+let anoAtual = 2026
+let anoNascimento = 2010;
+
+
+let idade = anoAtual - anoNascimento;
+console.log(idade);
+
