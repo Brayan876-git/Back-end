d6dcc774dd5db329e5cea30149ae59d692259d18
Brayan <brayanpontual73@gmail.com>
2026-09-25 09:31:34 -0300

    array-objeto

---
+++ AULA03/conceito_array.js
@@ -0,0 +1,26 @@
+// Array
+// um array é uma lista que pode armazenar vários valores.
+
+let frutas = ["Maçã", "Banana", "Uva"];
+
+console.log(frutas[1])
+
+// let frutas = ["0", "1", "2"]
+
+// Podemos adicionar itens!, sem alterar o array diretamente
+frutas.push("Laranja");
+console.log(frutas[3])
+
+// Remove itens do final dentro da Array (laranja [3])
+frutas.pop("Laranja");
+console.log(frutas);
+
+
+
+//============================================================
+// - Crie um array chamado `animais` e adicione três animais.
+// - Exiba o primeiro e o último no console.
+
+let animais = ["Cachorro","Gato", "Lontra"];
+console.log(animais[0]);
+console.log(animais[2]);
---
+++ AULA03/conceito_objeto.js
@@ -0,0 +1,21 @@
+// Object (Objetos) 
+// Um objeto agrupa várias informações dentro de uma mesma variável, usando chave e valor
+
+let pessoa = {
+    nome: "Ana",
+    idade: 17,
+    cidade: "São Paulo"
+};
+
+console.log(pessoa);
+
+// Crie um objeto chamado `carro` com as propriedades `marca`, `modelo` e `ano`.
+// Exiba cada propriedade no console.
+
+let carro = {
+    marca: "Mclaren",
+    modelo: "720s Spider",
+    ano: 2019
+};
+
+console.log(carro);
