4ed3c0ccbf3133ff4e546d577cf8350cb160e7b8
Brayan <brayanpontual73@gmail.com>
2026-09-25 13:52:55 -0300

    conceito estrutura condicional

---
+++ AULA04/conceito_estrutura_condicional.js
@@ -0,0 +1,29 @@
+// Estrutura Condicionais (Tomando Decisões)
+// As estruturas condicionais permitem executar blocos de código dependendo de uma condição
+
+// if/else - Condicionais 
+// If - Verifica se uma condição é verdadeira e executa o código dentro dele, se a condição for falsa , o Else pode executar outro bloco de código.
+
+let idade = 12;
+
+if (idade >= 18) {
+    console.log("Você é maior de idade")
+}  else{
+    console.log("Você é menor de idade");
+    
+}
+
+
+// If, Else If, Else (Multiplas Condições)
+let idade2 = 15;
+
+if (idade2 < 12){
+    console.log("Você é uma criança");
+    
+} else if (idade2 < 18) {
+    console.log("Você é um adolescente");
+    
+} else {
+    console.log("Você é um adulto");
+    
+}
